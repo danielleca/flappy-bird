e04d31c19203148cbfafef6f89826869f4c16954
danielleca <shuwen.campbell@icloud.com>
2025-09-11 19:27:02 +0100

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -1,5 +1,7 @@
 import pygame, sys
 pygame.init()
+flying=False
+gameover=False
 fps=60
 clock=pygame.time.Clock()
 screen=pygame.display.set_mode((800,750))
@@ -10,7 +12,7 @@ ground=pygame.image.load("ground.png")
 groundscroll=0
 pygame.display.update()
 class Bird(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y):
         super().__init__()
         self.images=[]
         self.index=0
@@ -18,10 +20,32 @@ class Bird(pygame.sprite.Sprite):
             img=pygame.image.load(f"bird{i}.png")
             self.images.append(img)
         self.image=self.images[self.index]
+        self.rect=self.image.get_rect()
+        self.rect.center=[x,y]
+        self.v=0
+        self.click=False
+    def update(self):
+        if flying==True:
+            self.v+=0.5
+            #gravity
+        if self.v>8:
+            self.v=8
+        if self.rect.bottom<600:
+            
+            self.rect.y+=self.v
+        self.index+=1
+        if self.index>=len(self.images):
+            self.index=0
+        self.image=self.images[self.index]
+bird=Bird(100,400)
+birdgrp=pygame.sprite.Group()
+birdgrp.add(bird)
 while True:
     clock.tick(fps)
     screen.blit(bg,(0,0))
     screen.blit(ground,(groundscroll,600))
+    birdgrp.draw(screen)
+    birdgrp.update()
     groundscroll-=3
     if abs(groundscroll)>35:
         groundscroll=0
@@ -29,3 +53,6 @@ while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+        if event.type==pygame.MOUSEBUTTONDOWN and flying==False and gameover==False:
+            flying=True
+    pygame.display.update()
